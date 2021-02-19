67af084fc522d39edf0a60f82df00ac23c911d3a
Craig <craig@craigsteel-design.com>
2021-02-19 08:02:44 +0000

    deleted js from all html files

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-function classToggle() {
-  const navs = document.querySelectorAll('.navbar__Items');
-
-  navs.forEach(nav => nav.classList.toggle('navbar__ToggleShow'));
-}
-
-document.querySelector('.navbar__Link-toggle')
-  .addEventListener('click', classToggle);
